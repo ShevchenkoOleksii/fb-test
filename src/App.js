6b8787d75b52b5279dff4cd2d579edfb01c4af4e
Oleksii Shevchenko <oleksiishevchenko93@gmail.com>
2022-08-12 17:40:40 +0300

    test v1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {HeadTags} from './HeadTags';
+const imageUrl = 'https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=';
+const { href } = window.location;
+const FB_DOMAIN = 'https://facebook.com/sharer.php?u=';
+
+const openPopup = (link, e) => {
+  e.preventDefault();
+  window.open(link, 'newwindow', 'width=300, height=250');
+};
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <HeadTags />
+      <div>
+        <img src={imageUrl} alt="bg"/>
+      </div>
+      <div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href={`${FB_DOMAIN}${href}`}
+          onClick={e => openPopup(`${FB_DOMAIN}${href}`, e)}
         >
-          Learn React
+          Share!
         </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ src/HeadTags.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import Helmet from "react-helmet";
+
+export const HeadTags = (props) => {
+	const imageUrl = 'https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=';
+	const {
+		title = "Portfolio - your name",
+		metaDescription = "default description",
+	} = props;
+	return (
+		<Helmet>
+			<title>{title}</title>
+			<meta name="viewport" content="initial-scale=1.0, width=device-width"/>
+			<meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+			<meta name="description" key="description" content={metaDescription}/>
+			<meta name="title" key="title" content={title}/>
+			<meta property="og:title" key="og:title" content={title}/>
+			<meta property="og:locale" key="og:locale" content="en_US"/>
+			<meta charSet="utf-8"/>
+			<meta property="og:type" key="og:type" content="website"/>
+			<meta
+				property="og:description"
+				key="og:description"
+				content={metaDescription}
+			/>
+			<meta
+				property="og:image"
+				key="og:image"
+				content={imageUrl}
+			/>
+		</Helmet>
+	);
+};
