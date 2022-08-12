0e4e3b385baa409c535026fa21d0f88780358ec2
Oleksii Shevchenko <oleksiishevchenko93@gmail.com>
2022-08-12 20:10:11 +0300

    test v1.10

--- src/HeadTags.js
+++ src/HeadTags.js
@@ -11,26 +11,26 @@ export const HeadTags = (props) => {
 	return (
 		<Helmet>
 			<title>{title}</title>
-			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
-			<meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-			<meta name="description" key="description" content={metaDescription} />
-			<meta name="title" key="title" content={title} />
-			<meta property="og:title" key="og:title" content={title} />
-			<meta property="og:locale" key="og:locale" content="en_US" />
-			<meta charSet="utf-8" />
-			<meta property="og:type" key="og:type" content="website" />
-			<meta
-				property="og:description"
-				key="og:description"
-				content={metaDescription} />
-			<meta
-				property="og:image"
-				key="og:image"
-				content="https://res.cloudinary.com/doghotelua/image/upload/v1581456521/blog/108_nft7ng.jpg" />
-			<meta property="og:image:alt" content="some alt text" />
-			<meta property="og:image:type" content="image/jpeg" />
-			<meta property="og:image:width" content="400" />
-			<meta property="og:image:height" content="300" />
+			{/*<meta name="viewport" content="initial-scale=1.0, width=device-width" />*/}
+			{/*<meta httpEquiv="X-UA-Compatible" content="IE=edge" />*/}
+			{/*<meta name="description" key="description" content={metaDescription} />*/}
+			{/*<meta name="title" key="title" content={title} />*/}
+			{/*<meta property="og:title" key="og:title" content={title} />*/}
+			{/*<meta property="og:locale" key="og:locale" content="en_US" />*/}
+			{/*<meta charSet="utf-8" />*/}
+			{/*<meta property="og:type" key="og:type" content="website" />*/}
+			{/*<meta*/}
+			{/*	property="og:description"*/}
+			{/*	key="og:description"*/}
+			{/*	content={metaDescription} />*/}
+			{/*<meta*/}
+			{/*	property="og:image"*/}
+			{/*	key="og:image"*/}
+			{/*	content="https://res.cloudinary.com/doghotelua/image/upload/v1581456521/blog/108_nft7ng.jpg" />*/}
+			{/*<meta property="og:image:alt" content="some alt text" />*/}
+			{/*<meta property="og:image:type" content="image/jpeg" />*/}
+			{/*<meta property="og:image:width" content="400" />*/}
+			{/*<meta property="og:image:height" content="300" />*/}
 		</Helmet>
 	);
 };
