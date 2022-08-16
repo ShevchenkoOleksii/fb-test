67c8d8a272118a826e0c19ffa1d70bcdcece5747
Oleksii Shevchenko <oleksiishevchenko93@gmail.com>
2022-08-16 13:29:01 +0300

    v2.2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-// import {HeadTags} from './HeadTags';
-const imageUrl = 'https://media.istockphoto.com/photos/hot-air-balloons-flying-over-the-botan-canyon-in-turkey-picture-id1297349747?b=1&k=20&m=1297349747&s=170667a&w=0&h=oH31fJty_4xWl_JQ4OIQWZKP8C6ji9Mz7L4XmEnbqRU=';
+import { Helmet } from 'react-helmet';
+
+const imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/15/08/blank-792125__340.jpg';
 const { href } = window.location;
 const FB_DOMAIN = 'https://facebook.com/sharer.php?u=';
 
@@ -9,13 +10,17 @@ const openPopup = (link, e) => {
   e.preventDefault();
   window.open(link, 'newwindow', 'width=300, height=250');
 };
-// const dataLink = 'https://developers.facebook.com/docs/plugins/';
 const dataLink = 'https://fb-test-app-v1.herokuapp.com/';
 
 function App() {
   return (
     <div className="App">
-      {/*<HeadTags />*/}
+      <Helmet>
+        <meta property="og:url" content={href} />
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="Hello World!"/>
+        <meta property="og:image" content="https://cdn.pixabay.com/photo/2015/05/31/15/08/blank-792125__340.jpg"/>
+      </Helmet>
       <div>
         <img src={imageUrl} alt="bg"/>
       </div>
@@ -27,52 +32,12 @@ function App() {
           Share!
         </a>
       </div>
-      {/*<iframe*/}
-      {/*  src={`https://www.facebook.com/plugins/share_button.php?href=${dataLink}&layout=button_count&size=small&width=77&height=20&appId`}*/}
-      {/*  width="77"*/}
-      {/*  height="20"*/}
-      {/*  scrolling="no"*/}
-      {/*  frameBorder="0"*/}
-      {/*  allowFullScreen="true"*/}
-      {/*  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"*/}
-      {/*>*/}
-      {/*</iframe>*/}
-      {/*<iframe*/}
-      {/*  src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fnotes-meneger-test.herokuapp.com%2Fapi%2Fauth&layout=button_count&size=small&width=77&height=20&appId"*/}
-      {/*  width="77"*/}
-      {/*  height="20"*/}
-      {/*  scrolling="no"*/}
-      {/*  frameBorder="0"*/}
-      {/*  allowFullScreen="true"*/}
-      {/*  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"*/}
-      {/*>*/}
-      {/*</iframe>*/}
-      {/*<iframe*/}
-      {/*  src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&layout=button_count&size=small&width=96&height=20&appId"*/}
-      {/*  width="96"*/}
-      {/*  height="20"*/}
-      {/*  scrolling="no"*/}
-      {/*  frameBorder="0"*/}
-      {/*  allowFullScreen="true"*/}
-      {/*  allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"*/}
-      {/*>*/}
-
-      {/*</iframe>*/}
-      {/*<div*/}
-      {/*  className="fb-share-button"*/}
-      {/*  data-href="https://developers.facebook.com/docs/plugins/"*/}
-      {/*  data-layout="button_count" data-size="small">*/}
-      {/*  <a target="_blank"*/}
-      {/*    href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&amp;src=sdkpreparse"*/}
-      {/*  >*/}
-      {/*    Поширити</a>*/}
-      {/*</div>*/}
       <div
         className="fb-share-button"
         data-href={dataLink}
         data-layout="button_count" data-size="small">
         <a target="_blank"
-           href={`https://www.facebook.com/sharer/sharer.php?u=${dataLink}&amp;src=sdkpreparse`}
+           href={`https://www.facebook.com/sharer/sharer.php?u=${dataLink}`}
         >
           Поширити</a>
       </div>
